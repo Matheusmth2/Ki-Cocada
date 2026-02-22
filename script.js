669252972e48344c6faad2419f10ebdd5a7c70e3
Matheus Medeiros <matheusmed03@gmail.com>
2026-02-22 15:08:20 -0300

    ajustando o CSS da section .princing, ajustando o h1 e h1

--- script.js
+++ script.js
@@ -7,18 +7,3 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburguer.classList.toggle('toggle');
     });
 });
-
-/**Ajustando automaticamente o tamanho do texto conforme o tamanho da tela.*/
-function adjustTextSize() {
-    const textElements = document.querySelectorAll('.responsive-text');
-    textElements.forEach(element => {
-        const screenWidth = window.innerWidth;
-        if (screenWidth >= 1024) {
-            element.style.fontSize = '1.5rem';
-        } else if (screenWidth >= 768) {
-            element.style.fontSize = '1.25rem';
-        } else {
-            element.style.fontSize = '1rem';
-        }
-    });
-}
