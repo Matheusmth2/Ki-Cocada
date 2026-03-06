0c87cd322c7f1884587528db2145a9c79a89ede9
Matheus Medeiros <matheusmed03@gmail.com>
2026-03-05 21:25:49 -0300

    Carrosel de imagens, colocando fotos e adicionando o código em JS

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburguer.classList.toggle('toggle');
     });
 });
+let slidesIndex = 1;
+showSlides(slidesIndex);
+
+function plusSlides(n) {
+    showSlides(slidesIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slidesIndex = n);
+}
+function showSlides(n) {
+    let slides = document.getElementsByClassName("carrousel-slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slidesIndex = 1 }
+    if (n < 1) { slidesIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slidesIndex - 1].style.display = "block";
+    dots[slidesIndex - 1].className += " active";
+}
+setInterval(() => {
+    showSlides(slidesIndex += 1);
+}, 5000);
